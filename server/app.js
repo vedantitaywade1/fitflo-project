8bedd2946c473da18d9338163957ee2bfa104973
Vedanti Taywadegit config --global user.email vedantitaywade8@gmail.com <vedantitaywade8@gmail.com>
2026-02-03 20:30:23 +0530

    Day 1: Project setup, Express server, Nodemon setup

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('FitFlo API is running 🚀');
+});
+
+module.exports = app;
